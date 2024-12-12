da942b041a1a7a85660660ec56f65252bb6c7312
Rainer Santos <121032030+Imbr3m@users.noreply.github.com>
2024-12-12 16:11:10 +0400

    prototype

--- A2 - DDA/Executable Project Code/app.py
+++ A2 - DDA/Executable Project Code/app.py
@@ -32,7 +32,50 @@ type2label = tk.Label(type_frame, text = "Type 2 Here", font = ("Futura", 12))
 type2label.grid(row = 0, column = 1)
 type_frame.grid(row = 3, column = 0)
 
+### Search Frame
+search_frame = tk.Frame(root, relief = tk.RAISED, borderwidth = 2)
 
+
+search_frame.columnconfigure([0,1,2,3], weight = 1)
+
+label = tk.Label(search_frame, text = "Left Arrow", font = ("Futura", 16))
+label.grid(row = 0, column = 0)
+
+label = tk.Label(search_frame, text = "Search Box\nwith Drop-down Menu?", font = ("Futura", 16))
+label.grid(row = 0, column = 1, columnspan = 2)
+
+label = tk.Label(search_frame, text = "Right Arrow", font = ("Futura", 16))
+label.grid(row = 0, column = 3)
+
+search_frame.grid(row = 0, column = 1, columnspan = 2, sticky = "ew")
+
+### Info Frame
+info_frame = tk.Frame(root, relief = tk.SUNKEN, borderwidth = 4)
+
+# Calling rowconfigure and columnfigure within the info_frame
+# causes the contents of the frame (label) to be centered within it.
+info_frame.rowconfigure([0,1,2], weight = 1)
+info_frame.columnconfigure([0,1], weight = 1)
+
+## Various Information
+
+# The Pokedex entry is long, so we'll let it take up two columns
+pokedex_entry = tk.Label(info_frame, text = "Pokedex Entry", font = ("Futura", 16))
+pokedex_entry.grid(row = 0, column = 0, columnspan = 2)
+
+height_entry = tk.Label(info_frame, text = "Height", font = ("Futura", 16))
+height_entry.grid(row = 1, column = 0)
+
+weight_entry = tk.Label(info_frame, text = "Weight", font = ("Futura", 16))
+weight_entry.grid(row = 1, column = 1)
+
+species_entry = tk.Label(info_frame, text = "Species", font = ("Futura", 16))
+species_entry.grid(row = 2, column = 0)
+
+catch_entry = tk.Label(info_frame, text = "Catch Rate", font = ("Futura", 16))
+catch_entry.grid(row = 2, column = 1)
+
+info_frame.grid(row = 1, rowspan = 3, column = 1, columnspan = 2, sticky = "nsew")
 
 # Run the App
 root.mainloop()
